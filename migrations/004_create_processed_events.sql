5ebad9b3c17f8c734b235e9ec05475d88a113be1
Anton Osipenko <ekt.promo.osipenko.a@gmail.com>
2026-06-23 16:46:30 +0300

    task8 загрузка эталонного решения с таски 7

--- migrations/004_create_processed_events.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-CREATE TABLE processed_events (
-                                  event_id VARCHAR(255) PRIMARY KEY,
-                                  event_type VARCHAR(100) NOT NULL,
-                                  processed_at TIMESTAMP WITH TIME ZONE NOT NULL
-);
-
--- +goose Down
-DROP TABLE IF EXISTS processed_events;
